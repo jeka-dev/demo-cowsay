724c18dccfc1e9a4448ad78140b0b9592b9acdf1
djeang <djeangdev@yahoo.fr>
2024-03-05 19:33:07 +0100

    first import

--- jeka-src/Build.java
+++ jeka-src/Build.java
@@ -1,13 +1,19 @@
 import dev.jeka.core.api.file.JkPathFile;
 import dev.jeka.core.api.file.JkZipTree;
+import dev.jeka.core.api.java.JkJavaProcess;
 import dev.jeka.core.api.project.JkProject;
 import dev.jeka.core.api.project.JkProjectSourceGenerator;
+import dev.jeka.core.api.system.JkLog;
+import dev.jeka.core.api.system.JkProcess;
 import dev.jeka.core.api.utils.JkUtilsPath;
+import dev.jeka.core.api.utils.JkUtilsString;
 import dev.jeka.core.api.utils.JkUtilsZip;
+import dev.jeka.core.tool.JkDoc;
 import dev.jeka.core.tool.KBean;
 import dev.jeka.core.tool.builtins.project.ProjectKBean;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 class Build extends KBean {
@@ -28,6 +34,25 @@ class Build extends KBean {
     public void copyCowFiles() {
         Path target = project.compilation.layout.resolveClassDir().resolve("cows");
         JkUtilsZip.unzipUrl(COW_ZIP_URL, "/cowsay-perl-master/cows", target);
+    }
+
+    @JkDoc("Build a native Image")
+    public void nativeImg() {
+        Path javaHome = JkJavaProcess.CURRENT_JAVA_HOME;
+        Path jar = project.artifactLocator.getMainArtifactPath();
+        String relTarget = JkUtilsString.substringBeforeLast(jar.toString(), ".jar");
+        Path target = Paths.get(relTarget).toAbsolutePath();
+        Path nativeImageExe = javaHome.resolve("bin/native-image");
+        JkProcess.of(nativeImageExe.toString(), "--no-fallback")
+                .addParams("-H:+UnlockExperimentalVMOptions")
+                .addParams("-H:IncludeResources=cows/.*$")
+                .addParams("-H:IncludeResourceBundles=MessagesBundle")
+                .addParams("-jar", jar.toString())
+                .addParams("-H:Name=" + target)
+                .setLogCommand(true)
+                .setInheritIO(true)
+                .exec();
+        JkLog.info("Generated in %s", target);
     }
 
 }
